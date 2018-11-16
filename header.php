2bcca72eaf3d5562b67ad84a3324a1f12834c384
sinamirzahosseinian <sina.mirza2@gmail.com>
2018-11-16 11:52:20 +0330

    version 1.0 completed

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="en-US">
+<head>
+    
+
+
+<img src="<?php echo Asset::image( 'shop/slider/1.jpg' ); ?>" alt="Shop Image">
+<script type="text/javascript" src="<?php echo Asset::js( 'plugins.js' ); ?>"></script>
+<link rel="stylesheet" href="<?php echo Asset::css( 'style.css' ); ?>" type="text/css"/>
+
+ </head>
